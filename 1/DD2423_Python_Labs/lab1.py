57dbc8749beb75ad2fc18b3b59731cdc7d9f897f
Stellarise <1151701517@qq.com>
2024-11-10 23:43:53 +0100

    1.4

--- 1/DD2423_Python_Labs/lab1.py
+++ 1/DD2423_Python_Labs/lab1.py
@@ -49,3 +49,54 @@ for row, (u, v) in enumerate(coordinates):
 
 # 一次性展示所有子图
 plt.show()
+
+
+# 定义图像
+F = np.concatenate([np.zeros((56, 128)), np.ones((16, 128)), np.zeros((56, 128))])
+G = F.T
+H = F + 2 * G
+
+# 计算傅里叶变换
+Fhat = np.fft.fft2(F)
+Ghat = np.fft.fft2(G)
+Hhat = np.fft.fft2(H)
+
+# 创建图像窗口，使用 3 行 3 列的网格布局
+fig, axs = plt.subplots(3, 3, figsize=(12, 12))
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+# 显示原始图像 F、G、H
+axs[0, 0].imshow(F, cmap='gray')
+axs[0, 0].set_title("Image F")
+
+axs[1, 0].imshow(G, cmap='gray')
+axs[1, 0].set_title("Image G")
+
+axs[2, 0].imshow(H, cmap='gray')
+axs[2, 0].set_title("Image H")
+
+# 显示 F、G、H 的傅里叶频谱
+axs[0, 1].imshow(np.log(1 + np.abs(Fhat)), cmap='gray')
+axs[0, 1].set_title("Fourier Spectrum of F")
+
+axs[1, 1].imshow(np.log(1 + np.abs(Ghat)), cmap='gray')
+axs[1, 1].set_title("Fourier Spectrum of G")
+
+axs[2, 1].imshow(np.log(1 + np.abs(Hhat)), cmap='gray')
+axs[2, 1].set_title("Fourier Spectrum of H")
+
+# 使用 fftshift 显示中心化的傅里叶频谱
+axs[0, 2].imshow(np.log(1 + np.abs(np.fft.fftshift(Fhat))), cmap='gray')
+axs[0, 2].set_title("Centered Fourier Spectrum of F")
+
+axs[1, 2].imshow(np.log(1 + np.abs(np.fft.fftshift(Ghat))), cmap='gray')
+axs[1, 2].set_title("Centered Fourier Spectrum of G")
+
+axs[2, 2].imshow(np.log(1 + np.abs(np.fft.fftshift(Hhat))), cmap='gray')
+axs[2, 2].set_title("Centered Fourier Spectrum of H")
+
+# 去掉多余的坐标轴
+for ax in axs.flat:
+    ax.axis('off')
+
+plt.show()
